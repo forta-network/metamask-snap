74cf765d7279a79f237accf83b5188808010a10c
Steven Landers <steven.landers@gmail.com>
2023-06-15 16:18:32 -0400

    update index.js

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,62 +1,42 @@
 import { OnTransactionHandler } from '@metamask/snaps-types';
 import { panel, text, heading } from '@metamask/snaps-ui';
 import { Json } from '@metamask/utils';
+import { ethers } from 'ethers'
 
-const labels = [
-  "scammer-eoa", "scammer-contract"
+interface ABIConfig {
+  abi: string
+  args: string[]
+}
+
+const ABIs: ABIConfig[] = [
+  {
+    abi: "function transfer(address to, uint amount) returns (bool)",
+    args: ["to"]
+  },
+  {
+    abi: "function transferFrom(address from, address to, uint amount)",
+    args: ["from", "to"]
+  },
+  {
+    abi: "function approve(address spender, uint256 amount)",
+    args: ["spender"]
+  }
 ]
 
 interface LabeledAddresses {
-  label: string;
-  addresses: { [address: string]: number };
+  address: string;
+  labels: string[];
 }
 
 interface AddressResponse {
   results: LabeledAddresses[];
 }
 
-const getScams = async (): Promise<AddressResponse> => {
-  const url = `https://api.forta.network/address-risk/addresses?labels=${labels.join(",")}`;
+const getScams = async (addrs: string[]): Promise<AddressResponse> => {
+  const url = `https://api.forta.network/address-risk/addresses?addresses=${addrs.join(",")}`;
   const resp = await fetch(url);
   return (await resp.json()) as AddressResponse;
 };
-
-function anyContains(haystacks: string[], needle: string): Boolean {
-  if (!needle) {
-    return false;
-  }
-  for (let haystack of haystacks) {
-    if (!haystack) {
-      continue;
-    }
-    const hs = haystack.replace(/0x/g, '').toLowerCase();
-    const nd = needle.replace(/0x/g, '').toLowerCase();
-    if (hs.indexOf(nd) >= 0) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function padData(data: string): string {
-  if (!data) {
-    return data;
-  }
-
-  // ensure data length-4 is evenly divisible by 32
-  const b = Buffer.from(data.replace('0x', ''), 'hex');
-  const padLevel = (b.length - 4) % 32;
-  if (padLevel == 0) {
-    return data;
-  }
-
-  // append 00 bytes
-  let suffix = '';
-  for (let i = 0; i < 32 - padLevel; i++) {
-    suffix += '00';
-  }
-  return `${data}${suffix}`;
-}
 
 function toPanel(map: { [key: string]: any }) {
   const result = []
@@ -65,6 +45,25 @@ function toPanel(map: { [key: string]: any }) {
     result.push(text(map[key]))
   }
   return panel(result)
+}
+
+function collectAddresess(data: string): { [key: string]: boolean } {
+  const dict: { [key: string]: boolean } = {}
+  for (const abiCfg of ABIs) {
+    try {
+      const iface = new ethers.utils.Interface([abiCfg.abi])
+      const res = iface.parseTransaction({ data })
+      for (const arg of abiCfg.args) {
+        const addr = res?.args[arg];
+        if (addr) {
+          dict[addr] = true
+        }
+      }
+    } catch (e) {
+      // squash, this just means it wasn't matched
+    }
+  }
+  return dict
 }
 
 export const onTransaction: OnTransactionHandler = async (args: {
@@ -79,23 +78,15 @@ export const onTransaction: OnTransactionHandler = async (args: {
   var insights: { [key: string]: any } = {};
 
   try {
-    const toAddress = transaction.to
-      ? (transaction.to as string).toLowerCase()
-      : '';
-    const data = padData(
-      transaction.data ? (transaction.data as string).toLowerCase() : '',
-    );
-
-    const scams = await getScams();
-
-    const badAddresses = [];
-    for (let result of scams.results) {
-      for (let addr in result.addresses) {
-        if (anyContains([data, toAddress], addr)) {
-          badAddresses.push(`${addr} (${result.addresses[addr]} scams)`);
-        }
-      }
+    let addresses: { [key: string]: boolean } = {
+      [transaction.to as string]: true,
     }
+    if (transaction.data) {
+      addresses = { ...addresses, ...collectAddresess(transaction.data as string) }
+    }
+
+    const scams = await getScams(Object.keys(addresses));
+    const badAddresses = scams.results.map((r) => r.address);
 
     if (badAddresses.length == 0) {
       insights['✅ Low Risk'] =
